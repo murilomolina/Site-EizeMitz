a0af0ce152bd5f78e99571df27c4fb249bc322d9
Murilo Molina Barone <murilo.m.barone@gmail.com>
2024-08-05 17:17:05 -0300

    botão de animação para exibir textos implementado para a index.html

---
+++ js/animacao.js
@@ -0,0 +1,20 @@
+// func para ativar a animção de texto via button
+document.addEventListener('DOMContentLoaded', () => {
+    const buttons = document.querySelectorAll('.toggle-btn');
+    
+    buttons.forEach(button => {
+        button.addEventListener('click', () => {
+            // Encontra o parágrafo associado ao botão clicado
+            const textContainer = button.nextElementSibling;
+
+            // Alterna a classe entre 'hidden' e 'visible'
+            if (textContainer.classList.contains('visible')) {
+                textContainer.classList.remove('visible');
+                textContainer.classList.add('hidden');
+            } else {
+                textContainer.classList.remove('hidden');
+                textContainer.classList.add('visible');
+            }
+        });
+    });
+});
